1352b06c873c1cf932e92fe12bc49eb7ae731488
IonAir1 <ionair.ironwood@gmail.com>
2023-06-17 22:10:47 +0800

    fixed bugs
    healing and freezing items were broken

--- scripts/main.gd
+++ scripts/main.gd
@@ -30,38 +30,34 @@ func _on_square_body_entered(body):
 			Global.health = 10
 		if body.freezing:
 			freeze()
+		if body.healing:
+			heal()
 	else:
 		Global.health -= 1
-		if not healing:
-			heal()
-		if body.freezing:
-			freeze()
 	body.queue_free()
 
 
 func _on_circle_body_entered(body):
 	if body.shape == 'circle':
 		Global.score += 1
+		if body.freezing:
+			freeze()
 		if body.healing:
 			Global.health = 10
 	else:
 		Global.health -= 1
-		if not healing:
-			heal()
-		if body.freezing:
-			freeze()
 	body.queue_free()
 
 
 func _on_triangle_body_entered(body):
 	if body.shape == 'triangle':
 		Global.score += 1
+		if body.freezing:
+			freeze()
 		if body.healing:
 			Global.health = 10
 	else:
 		Global.health -= 1
-		if not healing:
-			heal()
 	body.queue_free()
 
 
